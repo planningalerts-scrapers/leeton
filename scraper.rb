5db9143fa3e93f9490a9d9d37931d762d4b608ff
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 19:50:52 +1000

    Inline variable

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-url = "http://203.58.97.252/Horizon/@@horizondap@@/atdis/1.0/"
-
-ATDISPlanningAlertsFeed.save(url, "Sydney")
+ATDISPlanningAlertsFeed.save(
+  "http://203.58.97.252/Horizon/@@horizondap@@/atdis/1.0/",
+  "Sydney"
+)
